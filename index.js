caaa4ea04532030dcfdfdf5749e309f990138f1b
Tanvir Sakil <tanbirshakil18@gmail.com>
2021-12-23 19:08:58 +0600

    third commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { ObjectId } = require('mongodb');
 app.set('port', (process.env.PORT || 4000));
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sfmoe.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mxsrj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
